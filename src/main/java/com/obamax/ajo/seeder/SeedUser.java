cc4b0b5e7590663d0e5e9ff9238939d398e747d0
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2021-10-03 08:02:36 +0100

    seeded user now has admin role

--- src/main/java/com/obamax/ajo/seeder/SeedUser.java
+++ src/main/java/com/obamax/ajo/seeder/SeedUser.java
@@ -1,11 +1,18 @@
 package com.obamax.ajo.seeder;
 
+import com.obamax.ajo.exceptions.ResourceNotFoundException;
+import com.obamax.ajo.models.Role;
+import com.obamax.ajo.models.RoleType;
 import com.obamax.ajo.models.User;
+import com.obamax.ajo.repositories.RoleRepository;
 import com.obamax.ajo.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -13,6 +20,9 @@ public class SeedUser {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
 
     @Autowired
     PasswordEncoder passwordEncoder;
@@ -24,6 +34,11 @@ public class SeedUser {
             user1.setFirstName("Kanye");
             user1.setEmailAddress("kanyedonda@gmail.com");
             user1.setPassword(passwordEncoder.encode("donda100"));
+            List<Role> roles = new ArrayList<>();
+            Role role = roleRepository.findRoleByType(RoleType.ADMIN)
+                    .orElseThrow(() -> new ResourceNotFoundException("Error: Role is not found."));
+            roles.add(role);
+            user1.setRoles(roles);
             userRepository.save(user1);
             log.info("User seeded");
         }
