90b810f14a374b416b839abb77e418e9e90e65c7
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2021-10-11 15:21:53 +0100

    created editMember and getAllMembers method in memberService

--- src/main/java/com/obamax/ajo/services/MemberService.java
+++ src/main/java/com/obamax/ajo/services/MemberService.java
@@ -1,11 +1,15 @@
 package com.obamax.ajo.services;
 
 import com.obamax.ajo.models.Member;
+import com.obamax.ajo.payload.request.MemberEditRequest;
 import com.obamax.ajo.payload.request.RegisterMemberRequest;
+import java.util.List;
 
 
 public interface MemberService {
     Member registration(RegisterMemberRequest registerMember);
     Member saveMember(Member member);
     Member findMemberByEmail(String email);
+    Member editMember(Long memberId, MemberEditRequest memberRequest);
+    List<Member> getAllMembers();
 }
