dda23628ad28ea4e6f6d84d58588f3f084db2fe3
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2021-10-12 01:17:32 +0100

    created ContributionCycleService

---
+++ src/main/java/com/obamax/ajo/services/ContributionCycleService.java
@@ -0,0 +1,9 @@
+package com.obamax.ajo.services;
+
+import com.obamax.ajo.dto.ContributionCycleDTO;
+import com.obamax.ajo.models.ContributionCycle;
+
+public interface ContributionCycleService {
+
+    ContributionCycle createCycle(ContributionCycleDTO contributionCycleRequest);
+}
