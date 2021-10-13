e9c150759fdb175eac1303fc2b11a29a38533d77
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2021-10-13 18:56:42 +0100

    added two new methods

--- src/main/java/com/obamax/ajo/services/MemberService.java
+++ src/main/java/com/obamax/ajo/services/MemberService.java
@@ -1,6 +1,8 @@
 package com.obamax.ajo.services;
 
+import com.obamax.ajo.dto.RequestDTO;
 import com.obamax.ajo.models.Member;
+import com.obamax.ajo.models.Request;
 import com.obamax.ajo.payload.request.MemberEditRequest;
 import com.obamax.ajo.payload.request.RegisterMemberRequest;
 import java.util.List;
@@ -10,6 +12,8 @@ public interface MemberService {
     Member registration(RegisterMemberRequest registerMember);
     Member saveMember(Member member);
     Member findMemberByEmail(String email);
-    Member editMember(Long memberId, MemberEditRequest memberRequest);
+    Member editMember(Member member, MemberEditRequest memberRequest);
     List<Member> getAllMembers();
+    Request makeRequest(RequestDTO request, Long cycleId, String email);
+    Member findMemberById(Long memberId);
 }
