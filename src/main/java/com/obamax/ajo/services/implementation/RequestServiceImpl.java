900743153929187291e1bab8c9e6ce9b6c95258f
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2021-10-14 16:07:11 +0100

    created RequestServiceImpl class

---
+++ src/main/java/com/obamax/ajo/services/implementation/RequestServiceImpl.java
@@ -0,0 +1,50 @@
+package com.obamax.ajo.services.implementation;
+
+import com.obamax.ajo.dto.RequestDTO;
+import com.obamax.ajo.exceptions.BadRequestException;
+import com.obamax.ajo.models.ContributionCycle;
+import com.obamax.ajo.models.Member;
+import com.obamax.ajo.models.Request;
+import com.obamax.ajo.models.RequestStatusType;
+import com.obamax.ajo.repositories.ContributionCycleRepository;
+import com.obamax.ajo.repositories.MemberRepository;
+import com.obamax.ajo.repositories.RequestRepository;
+import com.obamax.ajo.services.RequestService;
+import com.obamax.ajo.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RequestServiceImpl implements RequestService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private ContributionCycleRepository contributionCycleRepository;
+
+    @Autowired
+    private RequestRepository requestRepository;
+
+    @Override
+    public Request makeRequest(RequestDTO requestDTO, Long cycleId, String email) {
+        Optional<Member> member = memberRepository.findByEmailAddress(email);
+        Optional<ContributionCycle> contributionCycle = contributionCycleRepository.findById(cycleId);
+
+        Request request = new Request();
+        if (member.isPresent() && contributionCycle.isPresent()) {
+            request.setMember(member.get());
+            request.setDateOfRequest(DateUtils.getCurrentTime());
+            request.setContributionCycle(contributionCycle.get());
+            request.setStatusOfRequest(RequestStatusType.PENDING);
+            request.setRequestMessage(requestDTO.getRequestMessage());
+
+            requestRepository.save(request);
+            return request;
+        } else {
+            throw new BadRequestException("ContributionCycle Id not valid and Member not found.");
+        }
+    }
+}
