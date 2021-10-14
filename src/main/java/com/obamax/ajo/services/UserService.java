9c042f834bb7aac6fe072315cd09c7bf6c08f961
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2021-10-14 16:06:15 +0100

    created member registration method in UserServiceImpl

--- src/main/java/com/obamax/ajo/services/UserService.java
+++ src/main/java/com/obamax/ajo/services/UserService.java
@@ -1,9 +1,12 @@
 package com.obamax.ajo.services;
 
+import com.obamax.ajo.models.Member;
 import com.obamax.ajo.models.User;
+import com.obamax.ajo.payload.request.RegisterMemberRequest;
 
 import java.util.Optional;
 
 public interface UserService {
     Optional<User> findUserByEmail(String email);
+    Member registration(RegisterMemberRequest registerMember);
 }
