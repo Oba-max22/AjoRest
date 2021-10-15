37340aa8a98ff33d76f1b1c2f9409139a1d08619
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2021-10-15 16:54:33 +0100

    created getAllCycleRequests method

--- src/main/java/com/obamax/ajo/services/RequestService.java
+++ src/main/java/com/obamax/ajo/services/RequestService.java
@@ -2,7 +2,9 @@ package com.obamax.ajo.services;
 
 import com.obamax.ajo.dto.RequestDTO;
 import com.obamax.ajo.models.Request;
+import java.util.List;
 
 public interface RequestService {
     Request makeRequest(RequestDTO request, Long cycleId, String email);
+    List<Request> getAllCycleRequests(Long cycleId);
 }
