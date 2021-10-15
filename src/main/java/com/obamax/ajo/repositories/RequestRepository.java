7e9294f8d123155250815b2f1241893f433d9552
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2021-10-15 21:00:14 +0100

    created findAllMember method in RequestRepository

--- src/main/java/com/obamax/ajo/repositories/RequestRepository.java
+++ src/main/java/com/obamax/ajo/repositories/RequestRepository.java
@@ -1,6 +1,7 @@
 package com.obamax.ajo.repositories;
 
 import com.obamax.ajo.models.ContributionCycle;
+import com.obamax.ajo.models.Member;
 import com.obamax.ajo.models.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,5 @@ import java.util.List;
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
     List<Request> findAllByContributionCycle(ContributionCycle contributionCycle);
+    List<Request> findAllByMember(Member member);
 }
