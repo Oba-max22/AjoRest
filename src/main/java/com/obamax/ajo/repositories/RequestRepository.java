fa521799438cf65aef153e3b03cfb9eb9f64f178
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2021-10-15 16:56:38 +0100

    created getAllCycleRequests method

--- src/main/java/com/obamax/ajo/repositories/RequestRepository.java
+++ src/main/java/com/obamax/ajo/repositories/RequestRepository.java
@@ -1,9 +1,12 @@
 package com.obamax.ajo.repositories;
 
+import com.obamax.ajo.models.ContributionCycle;
 import com.obamax.ajo.models.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
+    List<Request> findAllByContributionCycle(ContributionCycle contributionCycle);
 }
