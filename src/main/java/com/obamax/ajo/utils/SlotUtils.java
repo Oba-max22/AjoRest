f43ca49a7284fd53fd8d54faede46d1e34bb041a
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2021-10-20 14:08:11 +0100

    created SlotUtils method

---
+++ src/main/java/com/obamax/ajo/utils/SlotUtils.java
@@ -0,0 +1,10 @@
+package com.obamax.ajo.utils;
+
+
+public class SlotUtils {
+    public static int slotValue = 1;
+
+    public static int incrementSlotValue () {
+        return slotValue++;
+    }
+}
